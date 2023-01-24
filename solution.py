d82d7da91643a191daca5d3f2a73872c8c4639a0
Vincent Nilam <vincent.nilam@hotmail.com>
2023-01-24 16:57:47 +1100

    add code when properties aren't owned by anyone

--- solution.py
+++ solution.py
@@ -1,21 +1,28 @@
 import json
+import sys
+
+# Command line arguments
+
 
 # load board and rolls from command line*
-f = open('board.json')
+f = open(sys.argv[1])
 data = json.load(f)
 
-roll_1 = open('rolls_1.json')
-roll_data = json.load(roll_1)
+roll = open(sys.argv[2])
+roll_data = json.load(roll)
 
 print("BOARD: ")
 
-
 players = ['Peter', 'Billy', 'Charlotte', 'Sweedal'] 
 
-propertiesOwned = {'Peter': [], 'Billy': [], 'Charlotte': [], 'Sweedal': []}
+# dictionary to accumulate properties owned by each players
+properties_owned = {'Peter': [], 'Billy': [], 'Charlotte': [], 'Sweedal': []}
 
 # owned money is 16 at start.
-moneyOwned = {'Peter': 16, 'Billy': 16, 'Charlotte' :16, 'Sweedal': 16}
+money_owned = {'Peter': 16, 'Billy': 16, 'Charlotte' :16, 'Sweedal': 16}
+
+# initialise players' position
+players_position = {'Peter': 0, 'Billy': 0, 'Charlotte':0, 'Sweedal': 0}
 
 place = []
 for i in data:
@@ -25,9 +32,36 @@ for i in data:
 for i in range(len(place)):
     print(place[i])
 
+player_index = 0
+roll_index = 0
+
+winner = None
+
+while(winner is None):
+    player = players[player_index]
+    position = players_position[player]
+
+    # since everyone starts on GO, we just need to check go after the first roll
+    data_length = len(data)
+    
+    position = (position + roll_data[roll_index])%data_length 
+
+    # print(player, ' ',position)
+    if(data[position]['type']) == 'go':
+        money_owned[player] += 1
+    elif(data[position]['type'] == 'property'):
+        # check if the property is not owned by any players
+        if(data[position]['name'] not in properties_owned[player]):
+            # check if it's buyable; if it is, buy(put in properties_owned); else; bankrupt
+            if(money_owned[player] >= data[position]['price']):
+                properties_owned[player].append(data[position]['name'])
+                money_owned[player] -= data[position]['price']
+
+    #print(properties_owned[player])
+
 f.close()
 
 print()
 
 
-roll_1.close()
+roll.close()
